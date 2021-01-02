cdc5a56b30f0c939fc4319650afe1497e8b499ff
Yash-Jawale <yash25.j@gmail.com>
2021-01-02 20:05:15 +0530

    Added sass

--- js/main.js
+++ js/main.js
@@ -5,19 +5,19 @@ const burger = document.querySelector(".burger-cont");
 /* Toggle Mobile Menu */
 
 function toggleMenu() {
-	if (menu.classList.contains("active")) {
-		menu.classList.remove("active");
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
 
-		// adds the menu (hamburger) icon
-		// toggle.querySelector("a").innerHTML = `<i class="fas fa-bars"></i>`;
-	} else {
-		menu.classList.add("active");
+    // adds the menu (hamburger) icon
+    // toggle.querySelector("a").innerHTML = `<i class="fas fa-bars"></i>`;
+  } else {
+    menu.classList.add("active");
 
-		// adds the close (X) icon
-		// toggle.querySelector("a").innerHTML = `<i class="fas fa-times"></i>`;
-	}
+    // adds the close (X) icon
+    // toggle.querySelector("a").innerHTML = `<i class="fas fa-times"></i>`;
+  }
 
-	burger.classList.toggle("toggle-burger");
+  burger.classList.toggle("toggle-burger");
 }
 
 /* Event Listener */
@@ -29,36 +29,36 @@ const items = document.querySelectorAll(".item");
 
 /* Activate Submenu */
 function toggleItem() {
-	if (this.classList.contains("submenu-active")) {
-		this.classList.remove("submenu-active");
-	} else if (menu.querySelector(".submenu-active")) {
-		menu.querySelector(".submenu-active").classList.remove("submenu-active");
-		this.classList.add("submenu-active");
-	} else {
-		this.classList.add("submenu-active");
-	}
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
 }
 
 /* Event Listeners */
 
 if (document.documentElement.clientWidth < 960) {
-	// Check if user is on PC, disable click toggle functionality
-	for (let item of items) {
-		if (item.querySelector(".submenu")) {
-			item.addEventListener("click", toggleItem, false);
-			item.addEventListener("keypress", toggleItem, false);
-		}
-	}
+  // Check if user is on PC, disable click toggle functionality
+  for (let item of items) {
+    if (item.querySelector(".submenu")) {
+      item.addEventListener("click", toggleItem, false);
+      item.addEventListener("keypress", toggleItem, false);
+    }
+  }
 }
 
 /* Close Submenu from Anywhere */
 
 function closeSubmenu(e) {
-	let isClickInside = menu.contains(e.target);
+  let isClickInside = menu.contains(e.target);
 
-	if (!isClickInside && menu.querySelector(".submenu-active")) {
-		menu.querySelector(".submenu-active").classList.remove("submenu-active");
-	}
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
 }
 
 /* Event Listener */
@@ -69,14 +69,15 @@ document.addEventListener("click", closeSubmenu, false);
 const preloader = document.querySelector(".spinner-cont");
 
 window.addEventListener("load", () =>
-	preloader.classList.add("spinner-loaded")
+  // preloader.classList.add("spinner-loaded")
+  preloader.classList.remove("spinner-loading")
 );
 
 AOS.init({
-	// easing: "ease-in-out",
-	easing: "ease-out-quad",
-	duration: 1000,
-	// delay: 300,
-	once: true,
-	disable: "mobile",
+  // easing: "ease-in-out",
+  easing: "ease-out-quad",
+  duration: 1000,
+  // delay: 300,
+  once: true,
+  disable: "mobile",
 });
